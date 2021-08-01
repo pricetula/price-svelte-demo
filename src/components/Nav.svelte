717531393623bd6f4b819f24511bb15c1987679b
price tula <pricetulak@gmail.com>
2021-08-01 20:44:26 +0300

    added style

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,6 +3,14 @@
 import { component_subscribe } from "svelte/internal";
 
 </script>
-<nav>
+<nav class="nav">
   Nav component
 </nav>
+
+<style lang="scss">
+  @import "src/style/partial/index.scss";
+
+  .nav {
+    background-color: $midnight-blue;
+  }
+</style>
